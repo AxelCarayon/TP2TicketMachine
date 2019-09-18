2ba4c92ec239af54afcf63c6a1c8adfc8c43f25b
pedago <pedago@pcalb-mm0418.univ-jfc.fr>
2019-09-18 16:33:42 +0200

    Mise à jour de TicketMachine et du jeu de tests. 100% de couverture du code. Passe la totalité des tests.

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -29,5 +29,78 @@ public class TicketMachineTest {
 		machine.insertMoney(20);
 		assertEquals("La balance n'est pas correctement mise à jour", 10 + 20, machine.getBalance()); // Les montants ont été correctement additionnés               
 	}
+        
+        @Test
+        // S3 : On n'imprime pas le ticket si le montant inséré est insuffisant
+        public void noPrintTicketIfNotEnoughMoney() {
+            machine.insertMoney(40);
+            assertFalse("La machine imprime un ticket alors que le montant est insuffisant",machine.printTicket());
+        }
+        
+        @Test
+        //S4 : On imprime le ticket si le montant inséré est suffisant
+        public void printTicketIfEnoughMoney() {
+            machine.insertMoney(PRICE);
+            assertTrue("La machine n'imprime pas le ticket alors le montant est suffisant", machine.printTicket());
+        }
+        
+        @Test
+        //S5 : Quand on imprime un ticket la balance est décrémentée du prix du ticket
+        public void decrementBalanceWhenPrintingTicket(){
+            int reste = 10;
+            machine.insertMoney(PRICE+reste);
+            machine.printTicket();
+            assertEquals("Le prix du ticket imprimé n'as pas été décrémenté de la balance",reste,machine.getBalance());
+        }
+        
+        @Test
+        //S6 : Le montant colecté est mis à jour quand on imprime un ticket (pas avant)
+        public void priceAddedToTotalWhenPrinted(){
+            int ancienTotal = machine.getTotal();
+            machine.insertMoney(PRICE);
+            assertEquals("Le montant total a été mis à jour avant l'impression du ticket",ancienTotal,machine.getTotal());
+            machine.printTicket();
+            int nouveauTotal = ancienTotal + PRICE;
+            assertEquals("Le montant total n'as pas été mis correctement à jour après impression du ticket",nouveauTotal,machine.getTotal());
+        }
+        
+        @Test
+        //S7 : refund() rend correctement la monnaie
+        public void refundIsCorrect(){
+            int montantA = 10;
+            int montantB = 20;
+            int montantC = 40;
+            int montantInsereTotal = montantA + montantB + montantC;
+            machine.insertMoney(montantA);
+            machine.insertMoney(montantB);
+            machine.insertMoney(montantC);
+            assertEquals("Le montant rendu est différent du montant inséré",montantInsereTotal, machine.refund());
+        }
 
+        @Test
+        //S8 : refund() remet la balance à zéro
+        public void refundSetBalanceToZero(){
+            int montantA = 10;
+            int montantB = 20;
+            int montantC = 40;
+            int montantInsereTotal = montantA + montantB + montantC;
+            machine.insertMoney(montantA);
+            machine.insertMoney(montantB);
+            machine.insertMoney(montantC);  
+            machine.refund();
+            assertEquals("Rendre la monnaie ne remet pas correctement la balance à zéro",0,machine.getBalance());
+        }
+        
+        @Test (expected = IllegalArgumentException.class)
+        //S9 : On ne peut pas insérer de montant négatif
+        public void cantInsertNegativeAmount(){
+            machine.insertMoney(-10);
+        }
+        
+        @Test (expected = IllegalArgumentException.class)
+        //S10 : On ne peut pas créer de machine qui délivre des tickets dont le prix est négatif
+        public void cantInitializeWithNegativePrice(){
+            TicketMachine machineIncorrecte = new TicketMachine(-10);
+        }
+        
 }
